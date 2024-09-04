d93a4a554e74eb9fbc7d3de5995783ba35582c3a
itsmejaycabatuan <allenjaycabatuan@gmail.com>
2024-09-04 16:49:14 +0800

    update my repo again

--- Accounts.java
+++ Accounts.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package undang;
 
-/**
- *
- * @author SCC-LAB
- */
 public class Accounts {
+    int aid;
+    String fname, lname, email, user, pass;
     
+    public void addAccounts(int id, String Fname, String Lname, String emailAdd, String username, String password){
+        this.aid = id;
+        this.fname = Fname;
+        this.lname = Lname;
+        this.email = emailAdd;
+        this.user = username;
+        this.pass = password;
+    }
+    
+    public void viewAccounts(){
+        System.out.printf("%-5d %-10s %-10s %-20s %-10s %-10s\n",aid, fname, lname, email, user, pass);
+    }
 }
